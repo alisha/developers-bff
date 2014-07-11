9e7eb0544a97843a0ceb59d9314242ab6b2b6d24
Alisha Ukani <alishaaukani@gmail.com>
2014-07-10 22:28:10 -0400

    Create CSS file

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -4,6 +4,9 @@
 		<title>
 			@yield('title', 'Developers\' BFF')
 		</title>
+		<meta charset="UTF-8">
+
+		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 
 	<body>
--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -3,3 +3,4 @@
 @section('content')
 	<h1>Developers' BFF</h1>
 	<h2>Would you like to generate some <a href="lorem-ipsum">lorem ipsum text</a> or <a href="users">fake users</a>?</h2>
+@stop
